21c5e4c816936221a55b14b27d4aec39f4ecb6db
Diljam23 <156278364+Diljam23@users.noreply.github.com>
2025-04-12 22:33:58 -0500

    Update hotel-management-system.cpp

--- hotel-management-system.cpp
+++ hotel-management-system.cpp
@@ -4,13 +4,62 @@
 
 using namespace std;
 
-stuct Booking{
-    string guestName;
-    string roomType;
+class HotelSystem{
+Private:
+    struct Booking{
+    String name, roomType;
     int nights;
 };
 
-vector<Booking> bookings;
+vector <Booking> bookings;
+Public:
+    void run(){
+        int choice;
+        while(true){
+            showMenu();
+            choice =
+getUserChoice();
+            handlechoice(choice);
+            if (choice == 4)
+                break;
+        }
+}
+Private:
+    void showMenu() {
+        cout << "\n--- Hotel Menu ---\n"
+            <<"1. Book Room\n"
+            <<"2. View Bookings\n"
+            <<"3. Checkout\n"
+            <<"4. Exit\n"
+            <<"Choose:";
+}
+    int getUserChoice(){
+        int choice;
+        cin>> choice;
+        return choice;
+    }
+    void handleChoice(int choice){
+        switch (choice){
+            case 1: bookRoom();
+                break;
+            case 2: viewBookings();
+                break;
+            case 3: checkout();
+                break;
+            case 4: cout << "Goodbye!\n";
+                break;
+            default: cout << "Invalid option.\n";
+        }
+    }
+    void bookRoom(){
+        Booking b = getBookingDetails();
+        bookings.push_back(b);
+        cout<< "Booked for " << b.name << "!\n";
+    }
+    Booking getBookingDetails(){
+        Booking b;
+        cin.ignore();
+        cout <<
 void displayMenu(){
     cout<< "1. Book a room"<<endl;
     cout<<"2. View bookings"<<endl;
